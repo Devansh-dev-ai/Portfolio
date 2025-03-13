7054032ed4e41fb6f8111cb2d73fbb7925e57ba5
Devansh-dev-ai <141715459+Devansh-dev-ai@users.noreply.github.com>
2025-03-13 23:07:54 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -20,13 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l%peu=5=cgy3fds+wi-+z!$=2e$u2(58s5q^@vja(uofr@u5_$'
+# SECRET_KEY = 'django-insecure-l%peu=5=cgy3fds+wi-+z!$=2e$u2(58s5q^@vja(uofr@u5_$'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = ['devansh-durgapal.onrender.com']
-# ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+# ALLOWED_HOSTS = ['devansh-durgapal.onrender.com']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
